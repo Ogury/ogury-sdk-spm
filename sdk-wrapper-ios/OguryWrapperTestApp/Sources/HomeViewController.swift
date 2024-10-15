eeb440fe2ad1ab1224af18a64b1e6c6bafbf575d
zak <zakaria.chafik@ogury.co>
2024-10-15 10:53:46 +0200

    fix eerror

--- sdk-wrapper-ios/OguryWrapperTestApp/Sources/HomeViewController.swift
+++ sdk-wrapper-ios/OguryWrapperTestApp/Sources/HomeViewController.swift
@@ -127,5 +127,5 @@ extension HomeViewController : OguryInterstitialAdDelegate {
     
     func interstitialAdDidTriggerImpression(_ interstitialAd: OguryInterstitialAd) {
         print("interstitialAdDidTriggerImpression")
-    }    
+    }
 }
