ca3029ea904dcad792f6dafdbc108bbd87a1f863
Jerome Tonnelier <jerome.tonnelier@ogury.co>
2025-04-17 14:18:54 +0200

    Fiorst draft

---
+++ AdsCardAdapter/MaxCardAdapter/MaxCardAdapter/MaxCardAdapter.swift
@@ -0,0 +1,41 @@
+//
+//  MaxCardAdapter.swift
+//  MaxCardAdapter
+//
+//  Created by Jerome TONNELIER on 17/04/2025.
+//
+
+import AdsCardAdapter
+import AdsCardLibrary
+import OguryCore
+import UIKit
+
+struct MaxAdsCardAdapter: AdsCardAdaptable {
+    var assetKey: String
+    var options: AdapterOptions
+    var availableAdFormats: [AdsCardAdapter.AdAdapterFormatSection] = []
+    var sdkVersions: [String : String] = [:]
+    
+    func adManager(for adFormat: any AdAdapterFormat, options: AdViewOptions, viewController: UIViewController?, adDelegate: (any AdLifeCycleDelegate)?) throws(AdsCardAdapterError) -> any AdManager {
+        throw .noSuitableAdapterAvailable
+    }
+    
+    func adAdapterFormat(fromRawValue rawValue: Int) throws(AdsCardAdapterError) -> any AdAdapterFormat {
+        throw .noSuitableAdapterAvailable
+    }
+    
+    func startSdk() {
+        
+    }
+    
+    func resetSdk() {
+        
+    }
+    
+    func add(logger: any OguryLogger) {
+        
+    }
+    
+    
+}
+
