ac45f5a82eb6d46050f26ada5f38b9850f8ab0d8
Jerome Tonnelier <jerome.tonnelier@ogury.co>
2025-07-10 14:02:01 +0200

    Update core version

--- fastlane/configuration.rb
+++ fastlane/configuration.rb
@@ -22,7 +22,7 @@ class Configuration
     @deployment = Deployment.new(internalRepositories, publicRepositories)
     @slack = Slack.new("https://hooks.slack.com/services/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", "#sdk-ios-ci-update")
     @frameworks = Frameworks.new()
-    @frameworks.ogury_core = Framework.new("2.1.0-NewTestApp-1.0.2", "2.0.0", "2.0.0")
+    @frameworks.ogury_core = Framework.new("2.1.0-cocoapod-1.0.3", "2.0.0", "2.0.0")
     @frameworks.ogury_ads = Framework.new("4.1.0-NewTestApp-1.0.7", "4.0.0", "4.0.0")
     @frameworks.ogury_sdk = Framework.new("5.1.0-NewTestApp-1.0.8", "5.0.0", "5.0.0")
     @directories = Directories.new("./jenkins/build", "./jenkins/output", "./jenkins/test_derived_data", "./jenkins/testApp")
