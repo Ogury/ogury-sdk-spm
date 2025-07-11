4b67e976ba38b78afdc32174f05c5632c71c2d3d
Jerome Tonnelier <jerome.tonnelier@ogury.co>
2025-07-11 11:17:11 +0200

    Update wrapper version

--- fastlane/configuration.rb
+++ fastlane/configuration.rb
@@ -24,7 +24,7 @@ class Configuration
     @frameworks = Frameworks.new()
     @frameworks.ogury_core = Framework.new("2.1.0-cocoapod-1.0.3", "2.0.0", "2.0.0")
     @frameworks.ogury_ads = Framework.new("4.1.0-cocoapod-1.0.7", "4.0.0", "4.0.0")
-    @frameworks.ogury_sdk = Framework.new("5.1.0-NewTestApp-1.0.8", "5.0.0", "5.0.0")
+    @frameworks.ogury_sdk = Framework.new("5.1.0-cocoapod-1.0.0", "5.0.0", "5.0.0")
     @directories = Directories.new("./jenkins/build", "./jenkins/output", "./jenkins/test_derived_data", "./jenkins/testApp")
     prodTestApp = TestApplication.new("prodTestApp", "AdsTestApp-Prod", nil, "co.ogury.sdk.ads.app", "1:743372999564:ios:b2fa9c2a0751d1abca24a9")
     devcTestApp = TestApplication.new("devcTestApp", "AdsTestApp-Devc", nil, "co.ogury.sdk.ads.app.devc", "1:743372999564:ios:a479c7c9a882a87bca24a9")
