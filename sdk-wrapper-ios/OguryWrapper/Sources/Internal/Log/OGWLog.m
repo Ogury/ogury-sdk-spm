8c6736a833836c7ba007a0e41e23dec8858cc70f
Nicolas Perret <pernic75@gmail.com>
2025-07-25 10:40:58 +0200

    fix double log on new test app

--- sdk-wrapper-ios/OguryWrapper/Sources/Internal/Log/OGWLog.m
+++ sdk-wrapper-ios/OguryWrapper/Sources/Internal/Log/OGWLog.m
@@ -3,7 +3,7 @@
 //
 
 #import "OGWLog.h"
-#import <OguryCore/OguryOSLogger.h>
+#import <OguryCore/OguryNSLogger.h>
 #import <OguryCore/OguryLog.h>
 #import <OguryCore/OguryLogMessage.h>
 #import <OguryCore/OGCLog.h>
@@ -36,10 +36,10 @@ NSString *const OGWBundleIdentifier = @"com.ogury.OguryWrapper";
 
 - (instancetype)init {
    return [self init:[OGCLog shared].oguryLog
-            oSLogger:[[OguryOSLogger alloc] initWithSubSystem:OGWBundleIdentifier category:OGWLogOgury]];
+            nsLogger:[[OguryNSLogger alloc] init]];
 }
 
-- (instancetype)init:(OguryLog *)oguryLog oSLogger:(OguryOSLogger *)logger {
+- (instancetype)init:(OguryLog *)oguryLog nsLogger:(OguryNSLogger *)logger {
    if (self = [super init]) {
       _oguryLog = oguryLog;
       [_oguryLog addLogger:logger];
