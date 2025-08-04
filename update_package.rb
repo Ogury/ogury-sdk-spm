0f7cdcf3bd57dfa48fa34aa8019cac958c1de49f
Jerome Tonnelier <jerome.tonnelier@ogury.co>
2025-08-04 10:03:33 +0200

    try to update tag push script with Jenkins credentials

--- update_package.rb
+++ update_package.rb
@@ -94,8 +94,13 @@ if ARGV.include?("--tag")
   version = sdks.find { _1[:name] == "OguryWrapper" }[:version]
   tag = "v#{version}"
   puts "🏷️  Creating GitHub release with tag #{tag}..."
+
   system("git add Package.swift")
   system("git commit -m 'Update Ogury SDK to #{version}'")
+
+  https_remote = "https://#{ENV['GIT_USERNAME']}:#{ENV['GIT_TOKEN']}@github.com/Ogury/ogury-sdk-spm.git"
+  system("git remote set-url origin #{https_remote}")
+
   system("git tag #{tag}")
   system("git push origin #{tag}")
   puts "🚀 Release #{tag} pushed."
