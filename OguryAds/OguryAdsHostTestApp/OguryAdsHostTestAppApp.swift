8c009554752a95cd893716fce4a2d3d0074c078c
Jerome Tonnelier <jerome.tonnelier@ogury.co>
2025-09-30 10:49:33 +0200

    Ads host test app

---
+++ OguryAds/OguryAdsHostTestApp/OguryAdsHostTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OguryAdsHostTestAppApp.swift
+//  OguryAdsHostTestApp
+//
+// Copyright (c) 2025 and Confidential to ___ORGANIZATIONNAME___ All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct OguryAdsHostTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
