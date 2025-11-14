3f511a66483b6f6510c5ad097f9254ef53624b6c
Jerome Tonnelier <jerome.tonnelier@ogury.co>
2025-11-14 15:27:00 +0100

    Update package

--- Package.swift
+++ Package.swift
@@ -14,18 +14,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OguryWrapper",
-            url: "https://binaries.ogury.co/internal/prod/OgurySdk/OgurySdk-Prod-5.1.0-beta.cocoapod.1.zip",
-            checksum: "4d21a7bae79711c7f5537f7631ce4e2c23036e8aa08d57fba4c1d7b796943bfa"
+            url: "https://binaries.ogury.co/internal/prod/OgurySdk/OgurySdk-Prod-5.2.0-rc.5.zip",
+            checksum: "da4a2361d0c8a87c1d5a04466b814a4a9196deb5542b3cff6a6cc38f2a34e957"
         ),
         .binaryTarget(
             name: "OguryAds",
-            url: "https://binaries.ogury.co/internal/prod/OguryAds/OguryAds-Prod-4.1.0-beta.cocoapod.1.zip",
-            checksum: "a439f8f5eb278f046e02c9be5e0049d9bd2a2df6033561efdddce2fc07974bb7"
+            url: "https://binaries.ogury.co/internal/prod/OguryAds/OguryAds-Prod-4.2.0-rc.5.zip",
+            checksum: "ebba8fdf30ca65570bf7e68036257b40d41f361831bd180677fa58e6f0f75948"
         ),
         .binaryTarget(
             name: "OguryCore",
-            url: "https://binaries.ogury.co/internal/prod/OguryCore/OguryCore-Prod-2.1.0-beta.cocoapod.1.zip",
-            checksum: "8b3df0e05de8cc846712738e1c90b8324553fde61597cc9f644427104b4891e9"
+            url: "https://binaries.ogury.co/internal/prod/OguryCore/OguryCore-Prod-2.2.0-rc.2.zip",
+            checksum: "b3b3bb3ccaebb0a54cc23bf868e30641337ee10b106777f8684cf6a2b6ecb421"
         ),
         .binaryTarget(
             name: "OMSDK",
