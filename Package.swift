3b6178c2a90a91ec9232ab7db48f965ae6501bc1
Jerome Tonnelier <jerome.tonnelier@ogury.co>
2026-01-09 10:06:40 +0100

    release package.swift for 5.2.0 done

--- Package.swift
+++ Package.swift
@@ -14,23 +14,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OguryWrapper",
-            url: "https://binaries.ogury.co/internal/prod/OgurySdk/OgurySdk-Prod-5.2.0-rc.5.zip",
-            checksum: "da4a2361d0c8a87c1d5a04466b814a4a9196deb5542b3cff6a6cc38f2a34e957"
+            url: "https://binaries.ogury.co/release/ios/5.2.0/OgurySdk-5.2.0.zip",
+            checksum: "f9182da5915d175e4e0b7933802968cdcaef8a46427afb58d6945149bbdcb65a"
         ),
         .binaryTarget(
             name: "OguryAds",
-            url: "https://binaries.ogury.co/internal/prod/OguryAds/OguryAds-Prod-4.2.0-rc.5.zip",
-            checksum: "ebba8fdf30ca65570bf7e68036257b40d41f361831bd180677fa58e6f0f75948"
+            url: "https://binaries.ogury.co/release/ads-ios/4.2.0/OguryAds-4.2.0.zip",
+            checksum: "93865c7c13f05cdc98865be5ca888ae7df3ef7a6c02192a0e50c1c2ade211970"
         ),
         .binaryTarget(
             name: "OguryCore",
-            url: "https://binaries.ogury.co/internal/prod/OguryCore/OguryCore-Prod-2.2.0-rc.2.zip",
-            checksum: "b3b3bb3ccaebb0a54cc23bf868e30641337ee10b106777f8684cf6a2b6ecb421"
+            url: "https://binaries.ogury.co/release/core-ios/2.2.0/OguryCore-2.2.0.zip",
+            checksum: "3dcfdb1f4d74cd2aa1bcf9da970f45e9a557f7f3a0f5ffbe3ce6a37849b342a9"
         ),
         .binaryTarget(
             name: "OMSDK",
-            url: "https://binaries.ogury.co/internal/prod/OMSDK_Ogury/OMSDK_Ogury-Prod-4.1.0-beta.cocoapod.1.zip",
-            checksum: "9282081e270361b43d87dc222f8ad94cee5062df3ab248bfeeea40394361fe5e"
+            url: "https://binaries.ogury.co/release/omid-ios/4.2.0/OMSDK_Ogury-4.2.0.zip",
+            checksum: "7fc6d112a26b55f473dd8e512f49dbf9d37aeae3590b956419eb92235610302d"
         )
     ]
 )
